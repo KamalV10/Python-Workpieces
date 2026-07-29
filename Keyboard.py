992142fef022f8dd8168e1bb1dfaadf8dd7940b7
KamalV10 <valieff.KamalV@yandex.ru>
2026-07-29 16:41:55 +0300

    Update Keyboard.py

--- Keyboard.py
+++ Keyboard.py
@@ -1,1 +1,6 @@
 import keyboard as k #можно заменить k на что угодно или удалить as k
+def test(): # набор функций
+  k.press("b") # нажимает и удерживает b
+  k.release("b") # отпускает "b" если он нажат
+  k.press_and_release("a") # нажимает и отпускает "a"
+  k.write("launch") # пишет launch
